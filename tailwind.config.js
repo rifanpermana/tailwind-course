16683313f5b0c25d6855bd2e76d1c16b0178ef60
rifan <rifanpermana@gmail.com>
2023-04-04 02:04:19 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      animation: {
+        wavey: "wavey 1000ms linear infinite",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [
+    require("./plugins/openVariant"),
+    require("./plugins/animationDelay"),
+    require("./plugins/tableCaption"),
+    require("./plugins/buttonPlugin"),
+    require("./plugins/outlineSolid"),
+  ],
+};
